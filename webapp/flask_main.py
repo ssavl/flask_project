12230c78676f58eb837ac6135de7cf57e3ab973d
ssavl <get-moscow@yandex.ru>
2021-01-22 14:30:25 +0300

    added two form to the index.html, learn post method flask

--- webapp/flask_main.py
+++ webapp/flask_main.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template
 from weather import get_weather, URL
 from datetime import datetime
+from flask import request
+
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        print('пришел POST-запрос')
+        print('-' * 80)
+        print(request)
+    print('пришел GET-запрос')
+    if request:
+        print(request.form, request, dict(request.form))
+    else:
+        print('error')
     return render_template('index.html', weather=get_weather('Moscow'), page_var=str(datetime.now()))
 
 
